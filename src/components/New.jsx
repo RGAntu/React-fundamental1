9c70bc47f207c7db95710d4fbd864f7f4d78ec6a
RGAntu <rgantu887@gmail.com>
2026-01-15 23:03:00 +0600

    added ErrorBoundary to show error

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,4 +1,6 @@
+"use client"
 import React, { Suspense, use } from 'react';
+import {ErrorBoundary} from "react-error-boundary"
 
 const fetchPosts = fetch("https://jsonplaceholder.typicode.com/posts")
 .then(res => res.json())
@@ -6,9 +8,11 @@ const fetchPosts = fetch("https://jsonplaceholder.typicode.com/posts")
 const New = () => {
     return (
         <div>
+            <ErrorBoundary fallback={<h1 className='text-red-500'>Something went wrong...</h1>}>
             <Suspense fallback={<h1>Loading.......</h1>}>
                 <Posts fetchPosts={fetchPosts}></Posts>
             </Suspense>
+            </ErrorBoundary>
         </div>
     );
 };
