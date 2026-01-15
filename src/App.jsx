7921b7c5190e5d59c8818800171a4df47bd2bdea
RGAntu <rgantu887@gmail.com>
2026-01-15 21:15:26 +0600

    added useEffect side effect, dependency & rendering

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Products from './components/Products'
+
+function App() {
+
+  return (
+    <>
+     <Products></Products>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Products.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+const Products = () => {
+
+    const [product, setProduct] = useState( [] );
+
+
+
+    // normal data fetch 
+//     fetch('https://fakestoreapi.com/products')
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data);
+    //  setProduct(data) // commented this line because of unlimited execution or infinite loop cholbe.
+//   });
+
+
+
+//   useEffect( (callback function) => {
+
+//   }, [dependency array])
+// if we do not write dependency array this will re-execute again and again, dependency array means when UI first render after reload code will execute once
+
+  useEffect( () => {
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data);
+    setProduct(data) 
+  });
+
+  }, [])
+
+    console.log(product);
+
+
+
+    return (
+        <div>
+            <h2>Hello</h2>
+        </div>
+    );
+};
+
+export default Products;
