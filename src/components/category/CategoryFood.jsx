d33d9289157b32e1ec7f03948e5711c6c1aaebf4
RGAntu <rgantu887@gmail.com>
2026-01-20 20:31:25 +0600

    added handlewishlist btn

--- src/components/category/CategoryFood.jsx
+++ src/components/category/CategoryFood.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './Category.css'
 
 const CategoryFood = ({food}) => {
-
+    const handleWishList = () =>{
+        console.log("wishlist")
+    }
     return (
         <div>
             <img className='foodImg' src={food.strMealThumb} alt="" />
+            <h2>{food.strArea}</h2>
+            <p>{food.strCategory}</p>
+            <p>{food.strMeal}</p>
+            <button onClick={handleWishList} className='wishListBtn'>Wishlist</button>
         </div>
     );
 };
